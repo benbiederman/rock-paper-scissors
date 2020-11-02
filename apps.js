4572053598792e6942cd61b821b2965072e29783
benbiederman <benbiederman@gmail.com>
2020-11-02 06:58:28 -0600

    Update

--- apps.js
+++ apps.js
@@ -1,68 +1,120 @@
+const playerSelectionImg = document.querySelector('.player-throw');
+const cpuSelectionImg = document.querySelector('.cpu-throw');
+const playerWinCounter = document.querySelector('.player-wins')
+const tiesCounter = document.querySelector('.ties')
+const cpuWinCounter = document.querySelector('.cpu-wins')
+const popup = document.querySelector('.popup');
+const dismiss = document.querySelector('.dismiss')
+const gameResults = document.createElement('p');
+const playerWins = document.createElement('h3');
+const gameTies = document.createElement('h3');
+const cpuWins = document.createElement('h3');
+const playerOptions = document.querySelectorAll('.options div');
+
+let playerScore = 0;
+let cpuScore = 0;
+let tiesScore = 0;
+
+playerWins.textContent = playerScore;
+playerWinCounter.appendChild(playerWins);
+
+gameTies.textContent = tiesScore;
+tiesCounter.appendChild(gameTies);
+
+cpuWins.textContent = cpuScore;
+cpuWinCounter.appendChild(cpuWins);
+
+
+playerOptions.forEach((div) => {
+    div.addEventListener('click', (e) => {
+        let playerSelection = div.className;
+        playerSelectionImg.style.backgroundImage = `url(/img/${playerSelection}.jpg)`;
+        const cpuSelection = computerPlay();
+        cpuSelectionImg.style.backgroundImage = `url(/img/${cpuSelection}2.jpg)`;
+        setTimeout(() => {playRound(playerSelection, cpuSelection);}, 500);
+    })
+})
+
+function playRound(playerChoice, computerChoice){
+    if (playerChoice === computerChoice) {
+        popup.style.display = 'flex';
+        gameResults.textContent = `It's a tie!`;
+        popup.prepend(gameResults);
+        tiesScore++       
+    } else if(playerChoice === 'rock'){
+        if(computerChoice === 'paper'){
+            popup.style.display = 'flex';
+            gameResults.textContent = `Computer Wins! ${computerChoice} beats ${playerChoice}`;
+            popup.prepend(gameResults);
+            cpuScore++   
+        } else if(computerChoice === 'scissors'){
+            popup.style.display = 'flex';
+            gameResults.textContent = `Player Wins! ${playerChoice} beats ${computerChoice}`;
+            popup.prepend(gameResults);   
+            playerScore++
+        }
+    } else if(playerChoice === 'paper'){
+        if(computerChoice === 'scissors'){
+            popup.style.display = 'flex';
+            gameResults.textContent = `Computer Wins! ${computerChoice} beats ${playerChoice}`;
+            cpuScore++   
+            popup.prepend(gameResults);  
+        } else if(computerChoice === 'rock'){
+            popup.style.display = 'flex';
+            gameResults.textContent = `Player Wins! ${playerChoice} beats ${computerChoice}`;
+            popup.prepend(gameResults);   
+            playerScore++
+        }
+    } else if(playerChoice === 'scissors'){
+        if(computerChoice === 'rock'){
+            popup.style.display = 'flex';
+            gameResults.textContent = `Computer Wins! ${computerChoice} beats ${playerChoice}`;
+            cpuScore++   
+            popup.prepend(gameResults);  
+        } else if(computerChoice === 'paper'){
+            popup.style.display = 'flex';
+            gameResults.textContent = `Player Wins! ${playerChoice} beats ${computerChoice}`;
+            popup.prepend(gameResults);  
+            playerScore++ 
+        }
+    }
+
+    //Adds scores for player, CPU, and ties after each round
+    playerWins.textContent = playerScore;
+    playerWinCounter.appendChild(playerWins);
+
+    gameTies.textContent = tiesScore;
+    tiesCounter.appendChild(gameTies);
+
+    cpuWins.textContent = cpuScore;
+
+    cpuWinCounter.appendChild(cpuWins);
+
+    //Dismisses the popup, removes the selections
+    dismiss.addEventListener('click', (e) => {
+        popup.style.display = 'none'
+        cpuSelectionImg.style.backgroundImage = 'none';
+        playerSelectionImg.style.backgroundImage = 'none';
+        
+    })
+    
+}
+
+
+
+
+
+
 function computerPlay() {
     switch(Math.ceil(Math.random() * 3)){
         case 1:
-            return 'rock'
+            return 'rock';
             break;
-        case 2: 
-            return'paper'
+        case 2:
+            return 'paper';
             break;
         case 3:
-            return 'scissors'
+            return 'scissors';
             break;
     }
 }
-
-let playerScore = 0;
-let computerScore = 0;
-
-function game (){
-    const playerSelection = prompt('Rock, Paper, Scissors... SHOOT!');
-    const computerSelection = computerPlay();   
-    
-
-    if(playerSelection.toLowerCase() === computerSelection) {
-        console.log(`It's a draw! ${playerSelection.toLocaleLowerCase()} doesn't beat ${computerSelection}!`);
-    } else if(playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
-        console.log(`You lose! ${computerSelection} beats ${playerSelection.toLocaleLowerCase()}!`);
-        computerScore++;
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
-        console.log(`You win! ${playerSelection.toLocaleLowerCase()} beats ${computerSelection}!`)
-        playerScore++;
-    } else if (playerSelection.toLocaleLowerCase() === 'paper' && computerSelection === 'rock'){
-        console.log(`You win! ${playerSelection.toLocaleLowerCase()} beats ${computerSelection}!`);
-        playerScore++
-    } else if (playerSelection.toLocaleLowerCase() === 'paper' && computerSelection === 'scissors'){
-        console.log(`You lose! ${computerSelection} beats ${playerSelection.toLocaleLowerCase()}!`);
-        computerScore++
-    } else if (playerSelection.toLocaleLowerCase() === 'scissors' && computerSelection === 'paper'){
-        console.log(`You win! ${playerSelection.toLocaleLowerCase()} beats ${computerSelection}!`);
-        playerScore++;
-    } else if (playerSelection.toLocaleLowerCase() === 'scissors' && computerSelection === 'rock'){
-        console.log(`You lose! ${computerSelection} beats ${playerSelection.toLocaleLowerCase()}!`);
-        computerScore++
-    } else {
-        console.log('error! Shoot again');
-    }
-}
-
-for(i = 0; i < 5; i++){
-    game();
-}
-
-if(playerScore > computerScore){
-    alert(`Congratulations! You beat the computer with a score of ${playerScore} to ${computerScore}!`);
-} else if (computerScore > playerScore){
-    alert(`Sucks to suck! You lost to the computer with a score of ${playerScore} to ${computerScore}!`);
-} else if (computerScore === playerScore){
-    alert(`You jabronis tied with a score of ${playerScore} to ${computerScore}!`);
-} else {
-    alert(`Wut?`);
-}
-
-
-
-
-
-
-
-
